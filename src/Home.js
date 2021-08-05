592d30f574550d5c0b39a20b7fa4e0392ae2a6ac
mazenais <mazen.aboismail@gmail.com>
2021-08-05 13:42:54 +0200

    using useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 //outputting lists
 const Home = () => {
@@ -7,17 +7,24 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
      ]);
-
+const [name, setName] = useState('mario');
 const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 //using props to pass data from a parent component to a child component
 // passing functions as props (deleting components)
+//using efect
+useEffect(() => {
+    console.log('use effect ran')
+    console.log(name)
+}, [name]);
     return ( 
         <div className="home">
           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
           <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" handleDelete={handleDelete} />
+          <button onClick={() => setName('luigi')}>change name</button>
+          <p>{ name }</p>
         </div>
      );
 }
